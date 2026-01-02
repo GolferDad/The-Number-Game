044f579573c554efcb7c89e95313fb134dd3dab2
GolferDad <82014298+GolferDad@users.noreply.github.com>
2026-01-01 20:14:12 -0700

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import NumberGame from './NumberGame';
+import './index.css';
+
+function App() {
+  return (
+    <div className="app-root">
+      <NumberGame />
+    </div>
+  );
+}
+
+export default App;
+
+
+
